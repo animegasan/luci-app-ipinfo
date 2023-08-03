6de4ed6dd522dfcae6d697e197aa2545324f8d13
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-08-03 22:07:21 +0700

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,17 @@
+# See /LICENSE for more information.
+# This is free software, licensed under the GNU General Public License v2.
+
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=luci-app-ipinfo
+PKG_VERSION:=1.0.1-20230803
+PKG_MAINTAINER:=<https://github.com/animegasan>
+
+LUCI_TITLE:=LuCI for IP Geolocation API
+LUCI_DEPENDS:=+luci-base
+LUCI_PKGARCH:=all
+LUCI_DESCRIPTION:=LuCI support for IP Geolocation API
+
+include $(TOPDIR)/feeds/luci/luci.mk
+
+# call BuildPackage - OpenWrt buildroot signature
---
+++ htdocs/luci-static/resources/view/status/include/01_ipinfo.js
@@ -0,0 +1,27 @@
+'use strict';
+'require uci';
+'require view';
+
+return view.extend({
+  title:_('IP Information'),
+  handleSaveApply: null,
+  handleSave: null,
+  handleReset: null,
+  load: function() {
+    return Promise.all([
+      uci.load('ipinfo')
+    ]);
+  },
+  render: function(data) {
+    var sections = uci.sections('ipinfo');
+    var table = E('table',{'class':'table'});
+    table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width':'33%' }, ['IP Public']), E('td', {}, [sections[0].ip])]));
+    table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width':'33%' }, ['Provider']), E('td', {}, [sections[0].isp])]));
+    table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width':'33%' }, ['Region']), E('td', {}, [sections[0].city])]));
+    table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width':'33%' }, ['Country']), E('td', {}, [sections[0].country])]));
+    console.log(sections);
+    return table;
+  }
+  });
+
+
