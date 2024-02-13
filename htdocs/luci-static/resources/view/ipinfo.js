9524ba3e03aeed178abf55c4f771f4c760f6073f
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-02-14 06:21:24 +0700

    Update ipinfo.js

--- htdocs/luci-static/resources/view/ipinfo.js
+++ htdocs/luci-static/resources/view/ipinfo.js
@@ -1,3 +1,8 @@
+/* This is free software, licensed under the Apache License, Version 2.0
+ *
+ * Copyright (C) 2024 Hilman Maulana <hilman0.0maulana@gmail.com>
+ */
+
 'use strict';
 'require view';
 'require form';
@@ -5,49 +10,40 @@
 return view.extend({
 	render: function () {
 		var m, s, o;
-		m = new form.Map('ipinfo', _('IP Information'), _('Shows public ip information in Overview LuCI with ifconfig.co. <br/><br/><a href="https://github.com/animegasan" target="_blank">Powered by animegasan</a>'));
-
+		m = new form.Map('ipinfo', _('IP Information'), _('Shows public ip information in Overview LuCI with <a href="https://ip.guide/" target="_blank">ip.guide</a>.'));
 		s = m.section(form.TypedSection, 'ipinfo');
 		s.anonymous = true;
 
-		// Provider information
 		o = s.tab('isp', _('Provider Information'));
 		o = s.taboption('isp', form.Flag, 'ip', _('Public IP'));
 		o.default = '1';
 		o.rmempty = false;
-
-		o = s.taboption('isp', form.Flag, 'asn_org', _('Provider'));
+		o = s.taboption('isp', form.Flag, 'name', _('Provider'));
 		o.default = '1';
 		o.rmempty = false;
-
+		o = s.taboption('isp', form.Flag, 'organization', _('Organization'));
+		o.default = '1';
+		o.rmempty = false;
 		o = s.taboption('isp', form.Flag, 'asn', _('AS Number'));
 		o.default = '1';
 		o.rmempty = false;
-
-		// Location information
 		o = s.tab('loc', _('Location Information'));
 		o = s.taboption('loc', form.Flag, 'city', _('City'));
 		o.default = '1';
 		o.rmempty = false;
-
-		o = s.taboption('loc', form.Flag, 'region_name', _('Region'));
-		o.default = '1';
-		o.rmempty = false;
-
 		o = s.taboption('loc', form.Flag, 'country', _('Country'));
 		o.default = '1';
 		o.rmempty = false;
-
-		// Coordinate information
+		o = s.taboption('loc', form.Flag, 'timezone', _('Timezone'));
+		o.default = '1';
+		o.rmempty = false;
 		o = s.tab('co', _('Coordinate Information'));
 		o = s.taboption('co', form.Flag, 'latitude', _('Latitude'));
 		o.default = '1';
 		o.rmempty = false;
-
 		o = s.taboption('co', form.Flag, 'longitude', _('Longitude'));
 		o.default = '1';
 		o.rmempty = false;
-
 		return m.render();
 	},
 });
