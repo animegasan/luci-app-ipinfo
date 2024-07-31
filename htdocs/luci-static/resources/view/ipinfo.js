95d0cc990ca8f3b1ea81b601241dafefadd5a08d
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-07-31 20:07:08 +0700

    luci-app-ipinfo: Update ipinfo.js

--- htdocs/luci-static/resources/view/ipinfo.js
+++ htdocs/luci-static/resources/view/ipinfo.js
@@ -10,40 +10,36 @@
 return view.extend({
 	render: function () {
 		var m, s, o;
-		m = new form.Map('ipinfo', _('IP Information'), _('Shows public ip information in Overview LuCI with <a href="https://ip.guide/" target="_blank">ip.guide</a>.'));
-		s = m.section(form.TypedSection, 'ipinfo');
+		m = new form.Map('ipinfo', _('IP Information'),
+			_('Shows public IP information in Overview LuCI with <a %s>ip.guide</a>.').format('href="https://ip.guide" target="_blank"'));
+		s = m.section(form.NamedSection, 'config', 'ipinfo');
 		s.anonymous = true;
 
-		o = s.tab('isp', _('Provider Information'));
-		o = s.taboption('isp', form.Flag, 'ip', _('Public IP'));
-		o.default = '1';
+		o = s.option(form.Flag, 'enable', _('Enable'),
+			_('Enable or disable service.'));
 		o.rmempty = false;
-		o = s.taboption('isp', form.Flag, 'name', _('Provider'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.taboption('isp', form.Flag, 'organization', _('Organization'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.taboption('isp', form.Flag, 'asn', _('AS Number'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.tab('loc', _('Location Information'));
-		o = s.taboption('loc', form.Flag, 'city', _('City'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.taboption('loc', form.Flag, 'country', _('Country'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.taboption('loc', form.Flag, 'timezone', _('Timezone'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.tab('co', _('Coordinate Information'));
-		o = s.taboption('co', form.Flag, 'latitude', _('Latitude'));
-		o.default = '1';
-		o.rmempty = false;
-		o = s.taboption('co', form.Flag, 'longitude', _('Longitude'));
-		o.default = '1';
-		o.rmempty = false;
+
+		o = s.option(form.MultiValue, 'isp', _('Provider Information'),
+			_('Select ISP information to display.'));
+		o.display_size = '4';
+		o.value('ip', _('Public IP'));
+		o.value('name', _('Provider'));
+		o.value('organization', _('Organization'));
+		o.value('asn', _('AS Number'));
+
+		o = s.option(form.MultiValue, 'loc', _('Location Information'),
+			_('Select location information to display.'));
+		o.display_size = '3';
+		o.value('city', _('City'));
+		o.value('country', _('Country'));
+		o.value('timezone', _('Timezone'));
+
+		o = s.option(form.MultiValue, 'co', _('Coordinate Information'),
+			_('Select coordinate information to display.'));
+		o.display_size = '2';
+		o.value('latitude', _('Latitude'));
+		o.value('longitude', _('Longitude'));
+
 		return m.render();
 	},
 });
